7b34d942fa718cafc98b7a672faf0bb50be633da
Shailesh Jadav <jshailesh798@gmail.com>
2024-07-08 16:09:37 +0530

    add transaction model

---
+++ model/transaction.js
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose";
+
+const transactionSchema = new mongoose.Schema(
+   {
+      senderId: {
+         type: Number,
+         ref: "User",
+         required: true,
+      },
+
+      receiverId: {
+         type: Number,
+         ref: "User",
+         required: true,
+      },
+      title: {
+         type: String,
+         required: true,
+      },
+      amount: {
+         type: Number,
+         required: true,
+      },
+      createdAt: {
+         type: Date,
+         default: Date.now(),
+      },
+   },
+   { timestamps: true }
+);
+
+export const Transaction = mongoose.model("Transaction", transactionSchema);
