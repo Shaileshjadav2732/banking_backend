38ad1d556d7a6e111a2e6ba5fd86cf0f669fa880
Shailesh Jadav <jshailesh798@gmail.com>
2024-07-11 00:11:43 +0530

    PORT CHANGE

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ import { app } from "./app.js";
 import { connDb } from "./data/database.js";
 import { config } from "dotenv";
 
-const port = 5000;
+
 
 connDb();
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`server is working! on port ${port}`);
 });
 
